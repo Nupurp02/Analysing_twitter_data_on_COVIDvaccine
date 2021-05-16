b78a8a1686765de5fe6e2738d0edf075e6d874be
Nupur Patil <66691742+Nupurp02@users.noreply.github.com>
2021-05-16 19:52:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,5 +31,5 @@ def tweets():
 
 port = int(os.environ.get('PORT', 5000))
 app.run(host="0.0.0.0", port=port, debug=True)
-#if __name__ == '__main__':
-#    app.run()
+if __name__ == '__main__':
+    app.run()
